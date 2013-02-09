37be536190e93f2090ae5e8acc85cc13c65b213d
Dorrene Brown <dorreneb@gmail.com>
2013-02-08 19:42:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectjump::Application.config.secret_token = '5659b9eb7104cd4270e55dd540142ad47ac080006bec18fc1a3f68e665a7ec08bc0dd102b36dedad0b9fbdc35ee32a50865cd5827b80ebb7d51c5437dfd3c944'
