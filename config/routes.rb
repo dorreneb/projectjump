945d9ddd3680c1388c9aa4b101fcc041d280ab57
Dorrene Brown <dorreneb@gmail.com>
2013-02-08 23:17:28 -0500

    Pushing for andy.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Projectjump::Application.routes.draw do
+  devise_for :users
+
   get "welcome/index"
 
   resources :projects
@@ -55,7 +57,7 @@ Projectjump::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
-  root :to => 'projects#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
